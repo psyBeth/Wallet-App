18d73c224063ff0613b6be34dfc4aaf3ff5097be
Betul <betulkoru06@gmail.com>
2023-12-23 19:49:26 +0300

    javascript plan

---
+++ app.js
@@ -0,0 +1,17 @@
+//? selectors
+
+//? variables
+
+//? spreadsheet
+
+//? expenditure form
+
+//? expense table
+
+//? add income form
+
+//? When the page is first opened, it prints the data in localStorage and assigns it to variables.
+
+//? Making expense entries
+
+//? send the expense to Dom
